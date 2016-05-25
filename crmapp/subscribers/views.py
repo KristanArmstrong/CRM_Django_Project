d77707ada61a3ef31d64dcaa02f9d5b1e9701665
Kristan Armstrong <kristan.armstrong@gmail.com>
2016-05-25 14:52:44 -0700

    updated the subscriber view to use processing code

--- crmapp/subscribers/views.py
+++ crmapp/subscribers/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
 from django.http import HttpResponseRedirect
+from django.contrib.auth import authenticate, login
+from django.forms.forms import NON_FIELD_ERRORS
+from django.conf import settings
+from django.core.urlresolvers import reverse
 
 from .models import Subscriber
 from .forms import SubscriberForm
+
+import stripe
 
 def subscriber_new(request, template = 'subscribers/subscriber_new.html'):
 	if request.method == 'POST':
@@ -29,9 +35,33 @@ def subscriber_new(request, template = 'subscribers/subscriber_new.html'):
 									 city = city, state = state, user_rec = user)
 			sub.save()
 			#Process Payment (vis Stripe)
+			fee = settings.SUBSCRIPTION_PRICE
+			try:
+				stripe_customer = sub.charge(request, email, fee)
+			except stripe.StripeError as e:
+				form._errors[NON_FIELD_ERRORS] = form.error_class([e.args[0]])
+				return render(request, template, 
+					{'form': form,
+					 'STRIPE_PUBLISHABLE_KEY': settings.STRIPE_PUBLISHABLE_KEY}
+				)
 			#Auto Login
-			return HttpResponseRedirect('/success/')
+			a_u = authenticate(username = username, password = password) #auth user
+			if a_u is not None:
+				if a_u.is_active:
+					#successful login and returned the account list
+					login(request, a_u)
+					return HttpResponseRedirect(reverse('account_list'))
+				else:
+					#redirect to the login page
+					return HttpResponseRedirect(
+						reverse('django.contrib.auth.views.login')
+					)
+			else:
+				#redirect to the signup page
+				return HttpResponseRedirect(reverse('sub_new'))
 	else:
 		form = SubscriberForm() #user requests subscription for first time
 
-	return render(request, template, {'form' : form}) 
+	return render(request, template, 
+		{'form' : form,
+		 'STRIPE_PUBLISHABLE_KEY' : settings.STRIPE_PUBLISHABLE_KEY}) 

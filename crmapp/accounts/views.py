995757c5b5c743487b82c547cbb41ec7cd1bbab7
Kristan Armstrong <kristan.armstrong@gmail.com>
2016-05-25 16:44:19 -0700

    created account list view

--- crmapp/accounts/views.py
+++ crmapp/accounts/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 
-# Create your views here.
+from .models import Account
+
+class AccountList(ListView):
+	model = Account
+	template_name = 'accounts/account_list.html'
+	context_object_name = 'accounts'
+
+	def get_queryset(self):
+		#Overrides default behavior to only show records for current user
+		account_list = Account.objects.filter(owner = self.request.user)
+		return account_list
+
+	@method_decorator(login_required)
+	def dispatch(self, *args, **kwargs):
+		#Protects the view so only authenticated users can access it
+		return super(AccountList, self).dispatch(*args, **kwargs)

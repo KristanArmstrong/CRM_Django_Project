0145de607b8ccfc810d68c05c2fde3a42b179922
Kristan Armstrong <kristan.armstrong@gmail.com>
2016-05-25 15:16:33 -0700

    create login & logout URLS

--- crmapp/urls.py
+++ crmapp/urls.py
@@ -13,15 +13,14 @@ urlpatterns = patterns('',
     # Subscriber related URLs
     url(r'^signup/$', 'crmapp.subscribers.views.subscriber_new', name = 'sub_new'),
 
-
     # Admin URL
     url(r'^admin/', include(admin.site.urls)),
 
     # Login/Logout URLs
-
+    url(r'^login/$', 'django.contrib.auth.views.login', {'template_name' : 'login.html'}),
 
     # Account related URLs
-
+    url(r'^logout/$', 'django.contrib.auth.views.logout', {'next_page' : '/login/'}),
 
     # Contact related URLS
 

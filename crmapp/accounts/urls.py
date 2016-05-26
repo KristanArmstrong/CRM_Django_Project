67d4dbfed9bca1cd1f71516c9911b6431753cc02
Kristan Armstrong <kristan.armstrong@gmail.com>
2016-05-25 18:07:36 -0700

    added edit account URL

--- crmapp/accounts/urls.py
+++ crmapp/accounts/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import patterns, url
 account_urls = patterns('',
 
 	url(r'^$', 'crmapp.accounts.views.account_detail', name = 'account_detail'),
+	url(r'^edit/$', 'crmapp.accounts.views.account_cru', name = 'account_update'),
 
 )

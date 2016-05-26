278c713aa3b89a961d16ad75b38cfd3b405849e2
Kristan Armstrong <kristan.armstrong@gmail.com>
2016-05-26 02:07:43 -0700

    created the communications model

--- crmapp/communications/models.py
+++ crmapp/communications/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+from crmapp.accounts.models import Account
+
+from shortuuidfield import ShortUUIDField
+
+
+class Communication(models.Model):
+	uuid = ShortUUIDField(unique = True)
+	TYPE_LIST = (
+		(1, 'Meeting'),
+		(2, 'Phone'),
+		(3, 'Email'),
+	)
+	kind = models.PositiveSmallIntegerField(choices = TYPE_LIST)
+	date = models.DateField()
+
+	notes = models.TextField()
+	owner = models.ForeignKey(User)
+
+	subject = models.CharField(max_length = 50)
+	account = models.ForeignKey(Account)
+
+	created_on = models.DateField(auto_now_add = True)
+
+	class Meta:
+		verbose_name_plural = 'communications'
+
+	def __unicode__(self):
+		return u"%s" % self.subject
+
+	@models.permalink
+	def get_absolute_url(self):
+		return 'comm_detail', [self.uuid]
+
+	@models.permalink
+	def get_update_url(self):
+		return 'comm_update', [self.uuid]
+
+	@models.permalink
+	def get_delete_url(self):
+		return 'comm_delete', [self.id]

b005ff14d7f98127a4709218364788fd4bcb5b37
Kristan Armstrong <kristan.armstrong@gmail.com>
2016-05-26 19:45:01 -0700

    created the edit communication view

--- crmapp/communications/views.py
+++ crmapp/communications/views.py
@@ -1,10 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseForbidden, HttpResponseRedirect
 from django.core.urlresolvers import reverse
 
 from .models import Communication
 from .forms import CommunicationForm
+from crmapp.accounts.models import Account
 
 @login_required()
 def comm_detail(request, uuid):
@@ -16,10 +17,17 @@ def comm_detail(request, uuid):
 	return render(request, 'communications/comm_detail.html', {'comm' : comm})
 
 @login_required()
-def comm_cru(request):
+def comm_cru(request, uuid = None, account = None):
 	
+	if uuid:
+		comm = get_object_or_404(Communication, uuid = uuid)
+		if comm.owner != request.user:
+			return HttpResponseForbidden()
+	else:
+		comm = Communication(owner = request.user)
+
 	if request.POST:
-		form = CommunicationForm(request.POST)
+		form = CommunicationForm(request.POST, instance = comm)
 		if form.is_valid():
 			#make sure the user owns the account
 			account = form.cleaned_data['account']
@@ -35,11 +43,17 @@ def comm_cru(request):
 				args = (account.uuid,)
 			)
 			return HttpResponseRedirect(reverse_url)
+		else:
+			#if the form isn't valid, still fetch the account fo it can be passed
+			#to the template
+			account = form.cleaned_data['account']
 	else:
-		form = CommunicationForm()
+		form = CommunicationForm(instance = comm)
 
 	variables = {
 		'form' : form,
+		'comm' : comm,
+		'account' : account
 	}
 
 	template = 'communications/comm_cru.html'

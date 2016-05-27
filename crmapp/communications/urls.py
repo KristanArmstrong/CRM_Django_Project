7da93da12590f8f26081de37d363163238981d0a
Kristan Armstrong <kristan.armstrong@gmail.com>
2016-05-26 19:39:38 -0700

    created the edit communication URL

--- crmapp/communications/urls.py
+++ crmapp/communications/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import patterns, url
 comm_urls = patterns('',
 
 	url(r'^$', 'crmapp.communications.views.comm_detail', name = "comm_detail"), 
+	url(r'^edit/$', 'crmapp.communications.views.comm_cru', name = "comm_update"),
 
 )
